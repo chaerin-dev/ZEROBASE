69574a6dca8fdc1f7bac448624c02134d3c8772c
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-03-25 05:47:03 +0900

    16. 배열 내 최대값 구하기
    나의 풀이와 해답 추가

---
+++ DataStructure & Algorithm/16. 배열 내 최댓값 구하기 - 나의 풀이.js
@@ -0,0 +1,29 @@
+/* 16. 배열 내 최대값 구하기 */
+
+/* user code */
+function answer(arr) {
+  let max;
+
+  // 코드 구현 시작 영역
+  max = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    max = max > arr[i] ? max : arr[i];
+  }
+  // 코드 구현 종료 영역
+
+  return max;
+}
+
+/* main code */
+let input = [
+  // TC: 1
+  [1, 6, 5, 2, 3],
+  // TC: 2
+  [19, 41, 23, -4, 17],
+  // TC: 3
+  [-64, -27, -41, -33, -59],
+];
+
+for (let i = 0; i < input.length; i++) {
+  console.log(`#${i + 1} ${answer(input[i])}`);
+}
