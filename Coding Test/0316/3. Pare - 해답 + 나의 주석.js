eccd92852c4bab3a50444c2f0bd4f6703fe46102
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-03-25 16:59:24 +0900

    Upload 3. Pare
    나의 풀이, 해답+나의 주석 추가

---
+++ Coding Test/0316/3. Pare - 해답 + 나의 주석.js
@@ -0,0 +1,24 @@
+/**
+ * @param arr {number[]}
+ * @return {number}
+ */
+
+function solution(arr) {
+  // 주머니에 남은 구슬이 짝수개이면 술래가 구슬을 가져가지 않은 것
+  if (arr.length % 2 === 0) return 0;
+  // 주머니에 남은 구슬이 홀수개이면 술래가 구슬을 가져간 것
+  // 배열의 각 요소에 대해 콜백함수를 실행하는 .reduce() 메서드 사용
+  return arr.reduce((pre, cur) => {
+    console.log(pre, cur);
+    pre ^= cur;
+    return pre;
+  }, 0);
+}
+
+solution([1, 3, 2, 2, 1]);
+
+// 비트연산자 XOR(^)에 대하여..
+// 같은 두 수를 XOR하면 그 결과는 0이 된다.
+// 0과 어떤 수 A를 XOR하면 그 결과는 A가 된다.
+// 어떤 수 A에 어떤 수 B를 두 번 XOR하면 그 결과는 A가 된다.
+// 이런 특성을 이용하면 짝이 없는 수를 골라낼 수 있다..!
