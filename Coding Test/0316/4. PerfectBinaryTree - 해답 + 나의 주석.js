c16d49e207700fe47d1dec21eba73f0234ad2a36
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-03-25 17:19:34 +0900

    Edit 4. PerfectBinaryTree - 해답 + 나의 주석.js
    디버깅 코드 삭제

--- Coding Test/0316/4. PerfectBinaryTree - 해답 + 나의 주석.js
+++ Coding Test/0316/4. PerfectBinaryTree - 해답 + 나의 주석.js
@@ -13,8 +13,6 @@ function solution(n) {
   return result - 1;
 }
 
-console.log(solution(5));
-
 // 2^0 + 2^1 + ... + 2^(n-1) = 2^n - 1이라는 사실을 이용
 // 2^n을 구할 때에 한번에 구하지 않고,
 // 2를 곱하는 과정을 n번 반복하며 매번 MOD로 나누어줌!!
