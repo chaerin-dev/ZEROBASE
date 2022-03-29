ee8ee6494a9aa4b34febd6adf7416ceca86de117
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-03-29 09:11:05 +0900

    9. SecondLargestBinaryNumber
    나의 풀이, 해답+나의 주석 추가

--- Coding Test/0316/7. OlympicPlayedNumber - 해답 + 나의 주석.js
+++ Coding Test/0316/7. OlympicPlayedNumber - 해답 + 나의 주석.js
@@ -5,15 +5,15 @@
  */
 
 function solution(arr, num) {
-  // 2차원의 배열을 합칠 1차원 배열 선언
+  // 2차원 배열인 arr의 각 요소를 합칠 1차원 배열 선언
   let mergedArr = [];
-  // 각 국가의 선수 명단을 나타내는 arr의 각 요소를 mergedArr에 합쳐줌
+  // 각 국가의 선수 명단을 나타내는 arr의 각 요소를 mergedArr에 합침
   for (let i = 0; i < arr.length; i++) {
     mergedArr = [].concat(mergedArr, arr[i]);
   }
   // 1차원 배열 mergedArr를 오름차순으로 정렬
   mergedArr.sort((a, b) => a - b);
-  // Num번째 선수의 번호를 반환(배열 mergedArr의 인덱스는 0부터 시작하므로 Num-1 인덱스의 숫자 반환)
+  // Num번째 선수의 번호를 반환(배열 mergedArr의 인덱스는 0부터 시작하므로 Num-1 인덱스의 값 반환)
   return mergedArr[num - 1];
 }
 
