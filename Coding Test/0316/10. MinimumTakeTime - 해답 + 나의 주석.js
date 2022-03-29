d989ee5bf4ce8ae75eddae37a4f9fbb4e804bd3a
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-03-29 11:40:51 +0900

    add questions
    해답에 주석으로 문제 추가

--- Coding Test/0316/10. MinimumTakeTime - 해답 + 나의 주석.js
+++ Coding Test/0316/10. MinimumTakeTime - 해답 + 나의 주석.js
@@ -1,3 +1,17 @@
+/*
+병원에서 진료를 위해 예약 환자가 대기 중입니다. arr는 진료를 위해 대기 중인 환자들의 진료 부위 수를 나타냅니다.
+병원에는 n개만큼의 진료소가 있고, 1개의 진료 부위를 진료하는 데 1분이 소요된다고 가정합니다.
+이때 예약 환자가 모두 진료받는 데 걸리는 시간 분을 구하는 함수, solution을 완성해주세요.
+예를 들어, arr [1, 1, 1, 2, 2, 1, 1]가 있고, n이 3일 때, 모두 진료받는 시간은 6분입니다.
+> 0분에 진료소 [-, -, -] 전부 비어있고, arr는 [1, 1, 1, 2, 2, 1, 1]입니다.
+> 1분에 진료소 [1, 1, 1] 진료 부위가 남고, arr는 [2, 2, 1, 1]입니다.
+> 2분에 진료소 [-, -, -] 전부 비어있고, arr는 [2, 2, 1, 1]입니다.
+> 3분에 진료소 [2, 2, 1] 진료 부위가 남고, arr는 [1]입니다.
+> 4분에 진료소 [1, 1, -] 3 진료소가 비어있고, arr는 [1]입니다.
+> 5분에 진료소 [-, -, 1] 진료 부위가 남고, arr는 [] 입니다.
+> 6분에 모든 진료가 끝납니다.
+*/
+
 /**
  * @param arr {number[]}
  * @param n {number}
@@ -5,7 +19,7 @@
  */
 
 function solution(arr, n) {
-  // n개의 각 진료소의 상태를 저장할 Clinic을 생성
+  // n개 진료소의 상태를 저장할 clinic 생성
   const clinic = new Clinic(n);
   // clinic에 대해 Clinic 클래스의 getMinutes 메서드 수행
   return clinic.getMinutes(arr);
@@ -21,13 +35,17 @@ class Clinic {
     }
   }
 
+  // 총 소요 시간을 반환
   getMinutes(arr) {
     // 총 소요 시간을 저장할 변수 result
     let result = 0;
     // 예약 환자가 남아있거나 하나의 진료소라도 진료를 진행하고 있으면
     while (arr.length || !this.isDone()) {
+      // 빈 진료소에 환자 넣어주고
       this.enter(arr);
+      // 모든 진료소에서 1분동안 1개의 진료부위 진료
       this.heal();
+      // 총 소요시간 1 증가
       result++;
     }
     return result;
@@ -51,7 +69,6 @@ class Clinic {
   // 예약 환자가 남아있으면 가장 앞의 환자부터 차례로 빈 진료소에 넣어줌
   enter(arr) {
     if (!arr.length) return;
-
     for (const room of this.rooms) {
       if (room.isDone()) {
         room.enter(arr.shift());
