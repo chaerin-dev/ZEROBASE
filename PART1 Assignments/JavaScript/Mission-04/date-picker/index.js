2ab156efd7ff4751a575a30456d03ec6b4658db6
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-05-19 05:18:57 +0900

    Update Mission4 완성

--- PART1 Assignments/JavaScript/Mission-04/date-picker/index.js
+++ PART1 Assignments/JavaScript/Mission-04/date-picker/index.js
@@ -10,15 +10,15 @@ const calendarHTML = `
         <input type="text" class="date-picker-input" placeholder="Select date" readonly />
       </form>
       <div class="calendar">
-      <div class="calendar-nav">
-        <div class="button prev"></div>
-        <div class="now-info">
-          <div class="now-month"></div>
-          <div class="now-year"></div>
+        <div class="calendar-nav">
+          <div class="button prev"></div>
+          <div class="info">
+            <div class="month"></div>
+            <div class="year"></div>
+          </div>
+          <div class="button next"></div>
         </div>
-        <div class="button next"></div>
-      </div>
-      <div class="calendar-grid">
+        <div class="calendar-grid"></div>
       </div>`;
 
 const monthsArr = [
@@ -36,57 +36,138 @@ const monthsArr = [
   "December",
 ];
 const daysArr = ["SUN", "MON", "TUES", "WED", "THU", "FRI", "SAT"];
+
 const now = new Date();
-const year = now.getFullYear();
-const month = now.getMonth();
-const date = now.getDate();
-// const day = now.getDay();
+const [nowYear, nowMonth, nowDate] = [now.getFullYear(), now.getMonth(), now.getDate()];
 
 const DatePicker = ($container) => {
-  // const $input = $container.querySelector(".date-picker-input");
+  let [year, month] = [nowYear, nowMonth];
+  // let [showYear, showMonth] = [year, month];
+  let [selectedYear, selectedMonth, selectedDate] = [-1, -1, -1];
 
   const init = () => {
     $container.innerHTML = calendarHTML;
+  };
 
-    const $month = $container.querySelector(".now-month");
-    const $year = $container.querySelector(".now-year");
-    $month.innerHTML = `${monthsArr[month]}`;
-    $year.innerHTML = `${year}`;
+  const showCalendar = () => {
+    const $calendar = $container.querySelector(".calendar");
+    $calendar.classList.add("displayed");
+  };
 
-    const $calendar = $container.querySelector(".calendar-grid");
+  const hideCalendar = () => {
+    const $calendar = $container.querySelector(".calendar");
+    $calendar.classList.remove("displayed");
+  };
 
-    for (let i = 0; i < 7; i++) {
-      const $day = document.createElement("div");
-      $day.classList.add("day", `${daysArr[i]}`);
-      $day.innerHTML = `${daysArr[i]}`;
-      $calendar.appendChild($day);
-    }
-
-    const thisMonthFirstDate = new Date(year, month, 1);
-    const thisMonthLastDate = new Date(year, month + 1, 0);
-
-    for (let i = 0; i < thisMonthFirstDate.getDay(); i++) {
-      const $cell = document.createElement("div");
-      $cell.classList.add("date", `${daysArr[i]}`, "blurred");
-      $cell.innerHTML = `${thisMonthFirstDate.getDate()}`;
-      $calendar.appendChild($cell);
-    }
-    for (let i = 0; i < thisMonthLastDate.getDate(); i++) {
-      const $cell = document.createElement("div");
-      $cell.classList.add("date", `${daysArr[(thisMonthFirstDate.getDay() + i) % 7]}`);
-      $cell.innerHTML = `${i + 1}`;
-      if (i + 1 === date) $cell.style.border = "1px solid #55be77";
-      $calendar.appendChild($cell);
-    }
-    for (let i = 1; i < 7 - thisMonthLastDate.getDay(); i++) {
-      const $cell = document.createElement("div");
-      $cell.classList.add("date", `${daysArr[(thisMonthLastDate.getDay() + i) % 7]}`, "blurred");
-      $cell.innerHTML = `${i}`;
-      $calendar.appendChild($cell);
+  const calcPrevMonth = () => {
+    month--;
+    if (month < 0) {
+      year--;
+      month = 11;
     }
   };
 
+  const calcNextMonth = () => {
+    month++;
+    if (month > 11) {
+      year++;
+      month = 0;
+    }
+  };
+
+  const selectDate = (e) => {
+    selectedYear = parseInt(e.target.classList[1].slice(1));
+    selectedMonth = parseInt(e.target.classList[2].slice(1));
+    selectedDate = parseInt(e.target.classList[3].slice(1));
+    const dateStr = `${selectedYear}-${
+      selectedMonth + 1 < 10 ? "0" + (selectedMonth + 1) : selectedMonth + 1
+    }-${selectedDate < 10 ? "0" + selectedDate : selectedDate}`;
+    $container.querySelector("input").value = dateStr;
+    console.log(dateStr);
+  };
+
+  const renderCalendar = (year, month) => {
+    const $month = $container.querySelector(".month");
+    const $year = $container.querySelector(".year");
+    $month.innerHTML = `${monthsArr[month]}`;
+    $year.innerHTML = `${year}`;
+
+    let calendarGridInnerHTML = "";
+    for (let i = 0; i < 7; i++) {
+      calendarGridInnerHTML += `<div class="day ${daysArr[i]}">${daysArr[i]}</div>`;
+    }
+    const thisMonthFirstDate = new Date(year, month, 1);
+    const thisMonthLastDate = new Date(year, month + 1, 0);
+    const prevMonthLastDate = new Date(year, month, 0);
+    for (let i = thisMonthFirstDate.getDay(); i > 0; i--) {
+      const tempYear = month === 0 ? year - 1 : year;
+      const tempMonth = month === 0 ? 11 : month - 1;
+      const tempDate = prevMonthLastDate.getDate() - i + 1;
+      calendarGridInnerHTML += `<div class="date y${tempYear} m${tempMonth} d${tempDate} blurred">${tempDate}</div>`;
+    }
+    for (let i = 1; i <= thisMonthLastDate.getDate(); i++) {
+      calendarGridInnerHTML += `<div class="date y${year} m${month} d${i} ${
+        daysArr[(prevMonthLastDate.getDay() + i) % 7]
+      }">${i}</div>`;
+    }
+    for (let i = 1; i < 7 - thisMonthLastDate.getDay(); i++) {
+      const tempYear = month === 11 ? year + 1 : year;
+      const tempMonth = month === 11 ? 0 : month + 1;
+      calendarGridInnerHTML += `<div class="date y${tempYear} m${tempMonth} d${i} blurred">${i}</div>`;
+    }
+
+    const $calendarGrid = $container.querySelector(".calendar-grid");
+    $calendarGrid.innerHTML = calendarGridInnerHTML;
+
+    const $nowDate = $calendarGrid.querySelector(`.y${nowYear}.m${nowMonth}.d${nowDate}`);
+    if ($nowDate) $nowDate.classList.add("today");
+
+    const $selectedDate = $calendarGrid.querySelector(
+      `.y${selectedYear}.m${selectedMonth}.d${selectedDate}`
+    );
+    if ($selectedDate) $selectedDate.classList.add("selected");
+  };
+
   init();
+
+  $container.addEventListener("click", (e) => {
+    if (e.target.tagName === "INPUT") {
+      showCalendar();
+      if (selectedYear !== -1) {
+        renderCalendar(selectedYear, selectedMonth);
+      } else renderCalendar(nowYear, nowMonth);
+    }
+    if (e.target.classList.contains("prev")) {
+      calcPrevMonth();
+      renderCalendar(year, month);
+    }
+    if (e.target.classList.contains("next")) {
+      calcNextMonth();
+      renderCalendar(year, month);
+    }
+    if (e.target.classList.contains("date")) {
+      selectDate(e);
+      hideCalendar();
+    }
+  });
+
+  $container.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("date")) e.target.classList.add("hovered");
+  });
+
+  $container.addEventListener("mouseout", (e) => {
+    if (e.target.classList.contains("date")) e.target.classList.remove("hovered");
+  });
+
+  document.addEventListener("click", (e) => {
+    if (e.target.tagName === "INPUT") return;
+    if (e.target.classList.contains("date")) return;
+    if (e.target.parentNode.classList.contains("calendar")) return;
+    if (e.target.parentNode.classList.contains("calendar-nav")) return;
+    if (e.target.parentNode.classList.contains("info")) return;
+    if (e.target.parentNode.classList.contains("calendar-grid")) return;
+    hideCalendar();
+  });
 };
 
 export default DatePicker;
